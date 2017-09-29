52a0160af3905f6a5d5aa653bbf5ce933db889b7
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2017-09-29 09:29:57 +0700

    pergi

--- kantin.groovy
+++ kantin.groovy
@@ -6,19 +6,34 @@ class KantinDSLTest extends GroovyTestCase {
         KantinDSL.buat_transaksi {
             beli "ayam goreng"
             jumlah 1
-            status "take away"
+            status "dine in"
             uang 11000
             process
+            beli "ayam goreng"
+            jumlah 1
+            status "dine in"
+            uang 11000
+            process
+            beli "ayam goreng"
+            jumlah 1
+            status "dine in"
+            uang 11000
+            process
+            pergi "dine in"
             beli "ayam goreng"
             jumlah 1
             status "take away"
             uang 11000
             process
+            pergi "take away"
             beli "ayam goreng"
             jumlah 1
             status "take away"
             uang 11000
             process
+            pergi "dine in"
+            pergi "dine in"	
+            pergi "take away"
         }
     }
 }
@@ -34,7 +49,7 @@ class KantinDSL {
     int idx = -1
     def menu = ["ayam goreng", 'tahu', 'bebek']
     int quantity = -1
-    def stock = [2,3,1]
+    def stock = [3,3,1]
     int meja = 0
     int maksMeja = 2
     int uang = 5000
@@ -93,6 +108,21 @@ class KantinDSL {
         }
     }
 
+    def pergi(String wrapper){
+    	if (wrapper == 'dine in') {
+			if (this.meja > 0){
+	    		this.meja -= 1
+	    		println "Pelanggan yang melakukan dine in pergi\n"
+	    	}
+	    	else {
+	    		println "Command error. Jumlah meja = 0, operasi pergi invalid\n"
+	    	}
+    	}
+    	else {
+    		println "Pelanggan yang melakukan take away pergi\n"
+    	}
+    }
+
     def getProcess() {
         doProcess(this)
     }
@@ -100,22 +130,26 @@ class KantinDSL {
     private static doProcess(KantinDSL transaksi) {
         if (transaksi.status_pembelian == 0) {
             println "Pembelian Sukses"
-            println "Kembalian ${transaksi.kembalian}"
+            println "Kembalian ${transaksi.kembalian}\n"
         } else if (transaksi.status_pembelian == 1) {
             println "Pembelian Gagal"
-            println "Menu Tidak Ditemukan"
+            println "Menu Tidak Ditemukan\n"
         } else if (transaksi.status_pembelian == 2) {
             println "Pembelian Gagal"
-            println "Jumlah Stok Kurang"
+            println "Jumlah Stok Kurang\n"
         } else if (transaksi.status_pembelian == 3) {
             println "Pembelian Gagal"
-            println "Meja Penuh"
+            println "Meja Penuh\n"
         } else {
             println "Pembelian Gagal"
-            println "Uang Anda Kurang"
+            println "Uang Anda Kurang\n"
         }
     }
 
+
+}
+
+class Transaksi {
 
 }
 
@@ -134,13 +168,5 @@ class Makanan {
 }
 
 class BahanMakanan {
-
-}
-
-class Pesanan {
-
-}
-
-class Pembayaran {
 
 }
